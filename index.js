80aad565bb5b15c0d200e7c2c7b7dddaa30237b4
Maksim670051 <shcool.227@gmail.com>
2021-09-03 19:46:40 +0500

    Доработка программы
    Расширение функционала:
    1) Выводит произведение
    2) Выводит частное

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ function MathRez() {
     let number1 = Number(document.getElementById('number1').value);
     let number2 = Number(document.getElementById('number2').value);
 
-    document.getElementById('sum').innerHTML = number1 + number2
-    document.getElementById('raz').innerHTML = number1 - number2
-    document.getElementById('sr').innerHTML = ( Math.abs(number1) + Math.abs(number2) ) / 2
+    document.getElementById('sum').innerHTML = number1 + number2;
+    document.getElementById('raz').innerHTML = number1 - number2;
+    document.getElementById('sr').innerHTML = (Math.abs(number1) + Math.abs(number2)) / 2;
+    document.getElementById('pr').innerHTML = (Math.abs(number1) * Math.abs(number2));
+    document.getElementById('ch').innerHTML = (Math.abs(number1) / Math.abs(number2));
 
 }
